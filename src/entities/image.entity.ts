aab4c08fc503ac6d84675feba6eaa34908cf5ecb
Indra Fajar Nurwahid <fajarnurwahid99@gmail.com>
2025-09-07 07:06:21 +0700

    refactor(image): redesign image table
    - Improved image linking to violations
    - Removed redundant image_group_id
    - Added unique constraint to violation_id
    - Improved data integrity and efficiency
    - Updated related entities and services

--- src/entities/image.entity.ts
+++ src/entities/image.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { CommonBaseEntity } from './common-base.entity';
 import { Expose } from 'class-transformer';
+import { ImageLinks } from './image-links.entity';
 
 @Entity('images')
 export class ImageEntity extends CommonBaseEntity {
@@ -19,4 +20,10 @@ export class ImageEntity extends CommonBaseEntity {
 
   @Column()
   public size?: number;
+
+  @ManyToOne(() => ImageLinks, (imageLinks) => imageLinks.images, {
+    nullable: false,
+  })
+  @Expose({ name: 'image_link' })
+  public imageLink?: ImageLinks;
 }
