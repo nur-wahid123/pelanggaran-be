ecb07d1a0d2cb473f8d472ec37ccc8272ba32058
Indra Fajar Nurwahid <fajarnurwahid99@gmail.com>
2025-09-16 06:06:40 +0700

    feat(student-repository): implement student response DTO and enhance student query
    - Added StudentResponse DTO to include total points for students.
    - Updated findAllStudent method to return StudentResponse objects instead of StudentEntity.
    - Enhanced query to aggregate total points and include necessary joins for student and violation data.

--- src/repositories/student.repository.ts
+++ src/repositories/student.repository.ts
@@ -3,34 +3,120 @@ import { FilterDto } from 'src/commons/dto/filter.dto';
 import { PageOptionsDto } from 'src/commons/dto/page-option.dto';
 import { ClassEntity } from 'src/entities/class.entity';
 import { StudentEntity } from 'src/entities/student.entity';
+import { ViolationEntity } from 'src/entities/violation.entity';
 import { CreateStudentBatchDto } from 'src/modules/student/dto/create-student.dto';
+import { StudentResponse } from 'src/modules/student/dto/student-response.dto';
 import { DataSource, In, Repository } from 'typeorm';
 
 @Injectable()
 export class StudentRepository extends Repository<StudentEntity> {
-  findAllStudent(
+  async findAllStudent(
     query: FilterDto,
     pageOptionsDto: PageOptionsDto,
-  ): Promise<[StudentEntity[], number]> {
+  ): Promise<[StudentResponse[], number]> {
     const { search } = query;
     const { page, take, skip } = pageOptionsDto;
+
     const qb = this.createQueryBuilder('student')
-      .leftJoinAndSelect('student.violations', 'violations')
-      .leftJoinAndSelect('student.studentClass', 'studentClass')
-      .where((qb) => {
-        if (search) {
-          qb.andWhere('lower(student.name) LIKE lower(:search)', {
-            search: `%${search}%`,
-          });
-        }
+      .leftJoin('student.studentClass', 'studentClass')
+      .leftJoin('student.violations', 'violations')
+      .select([
+        'student.id',
+        'student.name',
+        'student.nationalStudentId',
+        'student.schoolStudentId',
+        'studentClass.id',
+        'studentClass.name',
+        'violations.id',
+      ])
+      .addSelect((subQuery) => {
+        return subQuery
+          .select('COALESCE(SUM(violation_type.point), 0)', 'totalPoints')
+          .from('violations', 'v')
+          .leftJoin(
+            'violations_violation_types_violation_types',
+            'vvt',
+            'vvt.violations_id = v.id',
+          )
+          .leftJoin(
+            'violation_types',
+            'violation_type',
+            'violation_type.id = vvt.violation_types_id AND violation_type.deleted_at IS NULL',
+          )
+          .leftJoin(
+            'violations_students_students',
+            'vss',
+            'vss.violations_id = v.id',
+          )
+          .where('vss.students_id = student.id')
+          .andWhere('v.deleted_at IS NULL');
+      }, 'student_totalPoints')
+      .where('student.deleted_at IS NULL');
+
+    if (search) {
+      qb.andWhere('LOWER(student.name) LIKE LOWER(:search)', {
+        search: `%${search}%`,
       });
+    }
+
+    qb.orderBy('"student_totalPoints"', 'DESC');
+    qb.addOrderBy('student.id', 'ASC');
 
     if (page && take) {
       qb.skip(skip).take(take);
     }
-    return qb.getManyAndCount();
-  }
 
+    // Group by necessary fields to prevent duplicates
+    qb.groupBy('student.id')
+      .addGroupBy('student.name')
+      .addGroupBy('student.nationalStudentId')
+      .addGroupBy('student.schoolStudentId')
+      .addGroupBy('studentClass.id')
+      .addGroupBy('violations.id')
+      .addGroupBy('studentClass.name');
+
+    const rawData = await qb.getRawMany();
+
+    // Map to store unique students by id
+    const studentMap = new Map<number, StudentResponse>();
+
+    for (const row of rawData) {
+      const studentId = row['student_id'];
+      if (!studentMap.has(studentId)) {
+        // Create a new student entity object
+        const student = new StudentResponse();
+        student.id = row['student_id'];
+        student.name = row['student_name'];
+        student.schoolStudentId = row['student_school_student_id'];
+        student.nationalStudentId = row['student_national_student_id'];
+        const studentClass = new ClassEntity();
+        studentClass.id = row['studentClass_id'];
+        studentClass.name = row['studentClass_name'];
+        student.studentClass = studentClass;
+        student.totalPoints = row['student_totalPoints'];
+        student.violations = [];
+        studentMap.set(studentId, student);
+      }
+      // If there is a violation, add it to the violations array (avoid duplicates)
+      if (row['violations_id']) {
+        const student = studentMap.get(studentId);
+        if (
+          !student.violations.some((v: any) => v.id === row['violations_id'])
+        ) {
+          const violations = student.violations;
+          const violation = new ViolationEntity();
+          violation.id = row['violations_id'];
+          violations.push(violation);
+          student.violations = violations;
+        }
+      }
+    }
+
+    const students = Array.from(studentMap.values());
+    const count = students.length;
+
+    return [students, count];
+  }
   async extractNisn(createStudentDto: CreateStudentBatchDto) {
     const { items } = createStudentDto;
     const nisns: string[] = [];
